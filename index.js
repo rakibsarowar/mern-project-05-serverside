0bfe70319d1220ae9df2633b4f0c1df0c93f3861
rakibsarowar <rakibsarowar@gmail.com>
2023-12-25 22:07:03 +0600

    working on user data

--- index.js
+++ index.js
@@ -26,11 +26,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("redCafe").collection("users");
     const menuCollection = client.db("redCafe").collection("menu");
     const reviewCollection = client.db("redCafe").collection("reviews");
     const cartCollection = client.db("redCafe").collection("carts");
 
+    // users related apis 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(item);
+      res.send(result);
+    })
 
+
+    // menu related apis
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
