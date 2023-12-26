9d543372f4a0159ca6fa787b28a1523e64a6ff69
rakibsarowar <rakibsarowar@gmail.com>
2023-12-26 23:04:38 +0600

    send jwt token to back end for verification

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ async function run() {
       res.send({ token });
     })
 
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
     // use verify admin after verifyToken
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
